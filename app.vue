9a30f175202714a20583cf8ac419c90f6125cefd
Marcus Loo <marcus@vecapital.asia>
2022-05-08 14:10:31 +0800

    initial commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <div>
+    <NuxtWelcome />
+  </div> -->
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Marcus Loo</title>
+    </head>
+    <body class="">
+      
+    </body>
+  </html>
+</template>
