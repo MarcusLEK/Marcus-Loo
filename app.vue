a6eca4c5e4507ad902c53f4b6786e5f2c7d96c87
Marcus Loo <marcus@vecapital.asia>
2022-05-08 22:14:45 +0800

    updated title

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marcus</title>
+    <title>Marcus Loo - Web Portfolio</title>
   </head>
   <body class="bg-raisinBlack">
     <!-- <div class="snap-y snap-mandatory h-screen overflow-scroll scroll-smooth"> -->
