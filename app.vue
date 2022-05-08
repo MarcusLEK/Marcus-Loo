e0d02d6f4940ab5c1ae141872368d066f2fdcd89
Marcus Loo <marcus@vecapital.asia>
2022-05-08 22:46:14 +0800

    added linkedin

--- app.vue
+++ app.vue
@@ -17,7 +17,7 @@
             </a>
           </div>
         </div>
-        
+
         <div class="snap-center h-screen flex items-center justify-center">
           <div>
             <span class="text-white text-5xl font-bold flex justify-center mb-5">ABOUT <span class="text-goGreen ml-2">ME</span></span>
@@ -182,6 +182,12 @@
                   </svg>
                   <span class="inline text-white text-xl my-auto ml-5">Marcuslooek@gmail.com</span>
                 </div>
+                <a href="linkedin.com/in/marcus-loo/" class="mb-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white inline" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" />
+                  </svg>
+                  <span class="inline text-white text-xl my-auto ml-5">linkedin.com/in/marcus-loo/</span>
+                </a>
                 <a href="https://drive.google.com/file/d/1yYHxsNtZJ4dXXBq1K9iRKLL6rCe66syB/view?usp=sharing" target="_blank" class="text-xl text-center text-white border-2 rounded-full p-5 cursor-pointer hover:border-goGreen transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 hover:bg-goGreen duration-300">
                   Download CV
                 </a>
